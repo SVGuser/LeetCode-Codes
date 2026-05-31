4ce6816e939895149f071d1723f94725c8af15f5
Shreshth Vardhan Godaria <105220448+SVGuser@users.noreply.github.com>
2026-05-31 15:11:00 +0530

    Link : https://leetcode.com/problems/destroying-asteroids/description/?envType=daily-question&envId=2026-05-31
    Implement asteroid destruction logic in Solution class with Python.

---
+++ Destroying_Asteroids.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        asteroids.sort()
+        for i in asteroids:
+            if mass >= i:
+                mass += i
+            else:
+                return False
+        return True
