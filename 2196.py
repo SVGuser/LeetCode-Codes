5e0126c2d06590537bde28a19246c23ab60419db
Shreshth Vardhan Godaria <105220448+SVGuser@users.noreply.github.com>
2026-06-07 23:16:31 +0530

    https://leetcode.com/problems/create-binary-tree-from-descriptions/description/?envType=daily-question&envId=2026-06-07
    Implementation of Binary Search Tree.

---
+++ 2196.py
@@ -0,0 +1,14 @@
+nodes = defaultdict(TreeNode)
+        children = set()
+        for parent, child, isLeft in descriptions:
+            if parent not in nodes:
+                nodes[parent] = TreeNode(parent)
+            if child not in nodes:
+                nodes[child] = TreeNode(child)
+            children.add(child)
+            if isLeft:
+                nodes[parent].left = nodes[child]
+            else:
+                nodes[parent].right = nodes[child]
+        root = (set(nodes.keys()) - children).pop()
+        return nodes[root]
