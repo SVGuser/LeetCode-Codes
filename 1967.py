0a15b6d5fdb76a4c567caadecc815567d261ead4
Shreshth Vardhan Godaria <105220448+SVGuser@users.noreply.github.com>
2026-06-29 23:42:40 +0530

    https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/description/?envType=daily-question&envId=2026-06-29
    Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
    
    A substring is a contiguous sequence of characters within a string.

---
+++ 1967.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        count = 0
+        for p in patterns:
+            if p in word:
+                count += 1
+        return count
